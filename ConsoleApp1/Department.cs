8e225d9a5a8b7261a5155112be452790083cec4a
Abbas0vv <ilhamabbasov46@gmail.com>
2025-04-05 02:13:38 +0400

    Task Completed

---
+++ ConsoleApp1/Department.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public int EmployeeLimit { get; set; }
+
+        public Employee[] Employees;
+
+        public Department(string name, int employeeLimit)
+        {
+            Name = name;
+            EmployeeLimit = employeeLimit;
+            Employees = new Employee[0];
+        }
+
+        public Employee this[int idx]
+        {
+            get { return Employees[idx]; }
+            set { Employees[idx] = value; }
+        }
+
+        public int Length {  get { return Employees.Length; } }
+
+        public void AddEmployee(Employee employee)
+        {
+            Array.Resize(ref Employees, Employees.Length + 1);
+            if (Employees.Length > EmployeeLimit)
+            {
+                throw new CapacityLimitException();
+            }
+            Employees[^1] = employee;
+        }
+    }
+}
---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Employee : IPerson
+    {
+        int _id;
+
+        public Employee(string name, byte age, double salary)
+        {
+            Id = _id;
+            Name = name;
+            Age = age;
+            Salary = salary;
+            _id++;
+        }
+
+        public int Id { get; }
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public double Salary { get; set; }
+
+        public string ShowInfo()
+        {
+            return $"{Id}. Dear {Name} ({Age}), your salary is {Salary}";
+        }
+
+        public override string ToString()
+        {
+            return ShowInfo();
+        }
+    }
+}
